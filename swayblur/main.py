55d1a596d058895c208b0577b0f85935d4e7bf76
Will McKinnon <contact@willmckinnon.com>
2021-11-10 00:45:36 -0500

    added animations

--- swayblur/main.py
+++ swayblur/main.py
@@ -6,15 +6,20 @@ import i3ipc
 
 BLUR_MIN = 5
 BLUR_MAX = 100
+
 CACHE_DIR = pathlib.Path.home() / '.cache/swayblur'
 
 
+def framePath(frame: int) -> str:
+    return '%s/%d.png' % (CACHE_DIR, frame)
+
+
 class blurWallpaper:
-    def __init__(self, wallpaperPath: str, blurStrength: int) -> None:
+    def __init__(self, wallpaperPath: str, blurStrength: int, animationDuration: int) -> None:
         self.SWAY = i3ipc.Connection()
         self.outputStatus = {}
         self.wallpaperPath = wallpaperPath
-        self.blurredWallpaperPath = '%s/%dblurredWallpaper.png' % (CACHE_DIR, blurStrength)
+        self.blurFrames = [(i + 1) * (blurStrength // animationDuration) for i in range(animationDuration)]
 
         self.genBlurredWallpaper(blurStrength)
         self.initOutputs()
@@ -36,10 +41,16 @@ class blurWallpaper:
 
     def genBlurredWallpaper(self, blurStrength: int) -> None:
         # TODO: better check
-        if not pathlib.Path(self.blurredWallpaperPath).is_file():
-            print('Generating blurred wallpaper...')
-            subprocess.run(['convert', self.wallpaperPath, '-blur', '0x%d' % blurStrength, self.blurredWallpaperPath])
-            print('Blurred wallpaper generated')
+        # TODO: threading
+        index = 1
+        for frame in self.blurFrames:
+            if not pathlib.Path(framePath(frame)).is_file():
+                print('Generating blurred wallpaper frame %d...' % index)
+                subprocess.run([
+                    'convert', self.wallpaperPath, '-blur', '0x%d' % frame, framePath(frame)
+                ])
+                index += 1
+        print('Blurred wallpaper generated')
 
 
     def isWorkspaceEmpty(self) -> None:
@@ -57,16 +68,19 @@ class blurWallpaper:
 
         if self.isWorkspaceEmpty() and self.outputStatus[focusedOutput]:
             # set wallpaper to original
+            for frame in reversed(self.blurFrames):
+                self.switchWallpaper(focusedOutput, framePath(frame))
             self.switchWallpaper(focusedOutput, self.wallpaperPath)
             self.outputStatus[focusedOutput] = False
 
         elif not self.isWorkspaceEmpty() and not self.outputStatus[focusedOutput]:
             # set wallpaper to blurred
-            self.switchWallpaper(focusedOutput, self.blurredWallpaperPath)
+            for frame in self.blurFrames:
+                self.switchWallpaper(focusedOutput, framePath(frame))
             self.outputStatus[focusedOutput] = True
 
 
-    def switchWallpaper(self, output, path) -> None:
+    def switchWallpaper(self, output: str, path: str) -> None:
         subprocess.run(['ogurictl', 'output', output, '--image', path])
 
 
@@ -80,6 +94,8 @@ def main() -> None:
     parser.add_argument('wallpaper_path', type=str)
     parser.add_argument('-b', '--blur', type=int, default=20,
             help='the blur strength (default: %(default)d, min: {}, max: {})'.format(BLUR_MIN, BLUR_MAX))
+    parser.add_argument('-a', '--animate', type=int, default=1,
+            help='animation duration (default: %(default)d)')
     args = parser.parse_args()
 
     # Validate args
@@ -89,9 +105,12 @@ def main() -> None:
     if args.blur < BLUR_MIN or args.blur > BLUR_MAX:
         print('Unable to run swayblur, blur is set to %d, which is not between 10-100' % args.blur)
         return
+    if args.animate < 1:
+        print('Unable to run swayblur, animate is set to %d, which is not a positive value' % args.animate)
+        return
 
     # Run blurring script
-    blurWallpaper(args.wallpaper_path, args.blur)
+    blurWallpaper(args.wallpaper_path, args.blur, args.animate)
 
 
 
