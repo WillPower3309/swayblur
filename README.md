5d137367b07d21d29136da39c1a2cc1795e99207
Will McKinnon <contact@willmckinnon.com>
2021-11-28 16:47:27 -0500

    initial commit

--- README.md
+++ README.md
@@ -7,18 +7,19 @@ Basic i3ipc based script to blur an output's wallpaper when a client is present 
 + `oguri`: used to set the wallpaper [without displaying a gray screen for a split second](https://github.com/swaywm/sway/issues/3693))
 
 ## Usage
-`swayblur WALLPAPER_PATH`
+`swayblur [-h] [-b BLUR] [-a ANIMATE]`
 
 | Option | Description |
 | ------ | ----------- |
-| `-b`, `--blur`    | blur strength (default: 20, min: 5, max: 100)    |
-| `-a`, `--animate` | animation duration (default: 1, min: 1, max: 20) |
-| `-h`, `--help`    | show the help message and exit                   |
+| `-b`, `--blur`        | blur strength (default: 20, min: 5, max: 100)                |
+| `-a`, `--animate`     | animation duration (default: 1, min: 1, max: 20)             |
+| `-c`, `--config-path` | oguri config path (default: $XDG\_CONFIG\_HOME/oguri/config) |
+| `-h`, `--help`        | show the help message and exit                               |
 
 **In order for the script to run as expected, your sway config should not set any wallpaper. Remove the `output * bg PATH` line.**
 
 ## Configuration
-While the use of this script requires no configuration, it does require `oguri`, which needs to be configured through its config file: `~/.config/oguri/config`. I personally use the below config, its about as minimal as you can get:
+Since swayblur requires `oguri`, it reads its config file: `~/.config/oguri/config`. I personally use the below config, it's about as minimal as you can get:
 
 ```
 [output *]
@@ -27,3 +28,4 @@ filter=nearest
 scaling-mode=fill
 anchor=center
 ```
+
--- swayblur/main.py
+++ swayblur/main.py
@@ -5,6 +5,7 @@ import shutil
 import json
 import multiprocessing
 import subprocess
+import configparser
 import i3ipc
 
 
@@ -15,19 +16,17 @@ ANIMATE_MAX = 20
 
 CACHE_DIR = pathlib.Path.home() / '.cache/swayblur'
 
-
 def parseArgs() -> bool:
     parser = argparse.ArgumentParser()
-    parser.add_argument('wallpaper_path', type=str)
     parser.add_argument('-b', '--blur', type=int, default=20,
-            help='the blur strength (default: %(default)d, min: {}, max: {})'.format(BLUR_MIN, BLUR_MAX))
+            help='The blur strength (default: %(default)d, min: {}, max: {})'.format(BLUR_MIN, BLUR_MAX))
     parser.add_argument('-a', '--animate', type=int, default=1,
-            help='animation duration (default: %(default)d, min: {}, max: {})'.format(ANIMATE_MIN, ANIMATE_MAX))
+            help='Animation duration (default: %(default)d, min: {}, max: {})'.format(ANIMATE_MIN, ANIMATE_MAX))
+    parser.add_argument('-c', '--config-path', type=str, default=pathlib.Path.home() / '.config/oguri/config',
+            help='Path to the oguri configuration file to use (default: %(default)s)')
     args = parser.parse_args()
 
     # Validate args
-    if not pathlib.Path(args.wallpaper_path).is_file():
-        parser.error('Unable to run swayblur, no such file "%s"' % args.wallpaper_path)
     if args.blur < BLUR_MIN or args.blur > BLUR_MAX:
         parser.error('Unable to run swayblur, blur is set to %d, which is not between %d-%d' % (args.blur, BLUR_MIN, BLUR_MAX))
     if args.animate < ANIMATE_MIN or args.animate > ANIMATE_MAX:
@@ -38,54 +37,29 @@ def parseArgs() -> bool:
     return args
 
 
-def validateCache(wallpaperPath: str, blurStrength: int, animationDuration: int) -> bool:
-    SETTINGS_FILE = CACHE_DIR / 'settings.json'
+def framePath(output: str, frame: int) -> str:
+    return '%s/%s-%d.png' % (CACHE_DIR, output, frame)
 
+
+def genFrame(wallpaperPath: str, output: str, frame: int) -> None:
     try:
-        with open(SETTINGS_FILE, 'r') as f:
-            settings = json.load(f)
-            if settings['blur'] == blurStrength and settings['animate'] == animationDuration and filecmp.cmp(settings['referenceFile'], wallpaperPath):
-                return True
-    except FileNotFoundError:
-        print('Creating cache directory: %s' % CACHE_DIR)
-        pass
-
-    shutil.rmtree(CACHE_DIR, ignore_errors=True)
-    CACHE_DIR.mkdir(parents=True)
-    referenceFile = shutil.copy(wallpaperPath, CACHE_DIR)
-    with open(SETTINGS_FILE, 'w') as f:
-        f.write(json.dumps({
-            'referenceFile': referenceFile,
-            'blur': blurStrength,
-            'animate': animationDuration,
-        }))
-    return False
-
-def framePath(frame: int) -> str:
-    return '%s/%d.png' % (CACHE_DIR, frame)
-
-
-def genFrame(wallpaperPath: str, frame: int) -> None:
-    try:
-        subprocess.run(['convert', wallpaperPath, '-blur', '0x%d' % frame, framePath(frame)])
+        subprocess.run(['convert', wallpaperPath, '-blur', '0x%d' % frame, framePath(output, frame)])
     except FileNotFoundError:
         print('Could not create blurred version of wallpaper, ensure imagemagick is installed')
         exit()
 
-    print('Generated frame %d' % frame)
+    print('Generated frame %s' % framePath(output, frame))
 
 
 class blurWallpaper:
-    def __init__(self, wallpaperPath: str, blurStrength: int, animationDuration: int, cached: bool) -> None:
+    def __init__(self, configPath: str, blurStrength: int, animationDuration: int, cached: bool) -> None:
         self.SWAY = i3ipc.Connection()
-        self.outputStatus = {}
-        self.wallpaperPath = wallpaperPath
+        self.outputs = self.initOutputs(configPath)
+        # TODO: self.focusedOutput
         self.blurFrames = [(i + 1) * (blurStrength // animationDuration) for i in range(animationDuration)]
 
         if not cached:
             self.genTransitionFrames()
-
-        self.initOutputs()
 
         self.handleBlur(self.SWAY, i3ipc.Event.WORKSPACE_INIT)
 
@@ -97,18 +71,48 @@ class blurWallpaper:
         self.SWAY.main()
 
 
-    def initOutputs(self) -> None:
-        self.outputStatus = {}
-        outputs = self.SWAY.get_outputs()
-        for output in outputs:
-            self.outputStatus[output.name] = False
+    def initOutputs(self, configPath: str) -> None:
+    # TODO: add file not exist check
+        config = configparser.ConfigParser()
+        config.read(configPath)
+
+        # init outputs to their defaults
+        outputs = {}
+        for output in i3ipc.Connection().get_outputs():
+            outputs[output.name] = {
+                'image': '',
+                'filter': '',
+                'anchor': '',
+                'scaling-mode': 'fill',
+                'is-blurred': False
+            }
+
+        # iterate through each output in the config
+        # TODO: this could probably be faster
+        for section in config.sections():
+            outputName = section.split('output ')[-1]
+            outputsToUpdate = []
+
+            if outputName == '*':
+                for output in outputs:
+                    if outputs[output]['image'] == '':
+                        outputsToUpdate.append(output)
+            else:
+                outputsToUpdate.append(outputName)
+
+            for output in outputsToUpdate:
+                for key in config[section]:
+                    outputs[output][key] = config[section][key]
+
+        return outputs
 
 
     def genTransitionFrames(self) -> None:
         print('Generating blurred wallpaper frames')
         print('This may take a minute...')
-        with multiprocessing.Pool() as pool:
-            pool.starmap(genFrame, [[self.wallpaperPath, frame] for frame in self.blurFrames])
+        for output in self.outputs:
+            with multiprocessing.Pool() as pool:
+                pool.starmap(genFrame, [[self.outputs[output]['image'], output, frame] for frame in self.blurFrames])
         print('Blurred wallpaper generated')
 
 
@@ -125,23 +129,32 @@ class blurWallpaper:
                 focusedOutput = output.name
                 break
 
-        if self.isWorkspaceEmpty() and self.outputStatus[focusedOutput]:
+        if self.isWorkspaceEmpty() and self.outputs[focusedOutput]['is-blurred']:
             # set wallpaper to original
             for frame in reversed(self.blurFrames):
-                self.switchWallpaper(focusedOutput, framePath(frame))
-            self.switchWallpaper(focusedOutput, self.wallpaperPath)
-            self.outputStatus[focusedOutput] = False
+                self.switchWallpaper(focusedOutput, framePath(focusedOutput, frame))
+            self.switchWallpaper(focusedOutput, self.outputs[focusedOutput]['image'])
+            self.outputs[focusedOutput]['is-blurred'] = False
 
-        elif not self.isWorkspaceEmpty() and not self.outputStatus[focusedOutput]:
+        elif not self.isWorkspaceEmpty() and not self.outputs[focusedOutput]['is-blurred']:
             # set wallpaper to blurred
             for frame in self.blurFrames:
-                self.switchWallpaper(focusedOutput, framePath(frame))
-            self.outputStatus[focusedOutput] = True
+                self.switchWallpaper(focusedOutput, framePath(focusedOutput, frame))
+            self.outputs[focusedOutput]['is-blurred'] = True
 
 
-    def switchWallpaper(self, output: str, path: str) -> None:
+    def switchWallpaper(self, outputName: str, path: str) -> None:
+        output = self.outputs[outputName]
+
         try:
-            subprocess.run(['ogurictl', 'output', output, '--image', path])
+            subprocess.run([
+                'ogurictl',
+                'output', outputName,
+                '--image', path,
+                '--filter', output['filter'],
+                '--anchor', output['anchor'],
+                '--scaling-mode', output['scaling-mode']
+            ])
         except:
             print('Could not set wallpaper, ensure oguri is installed')
             exit()
@@ -151,11 +164,8 @@ def main() -> None:
     # Parse arguments
     args = parseArgs()
 
-    # Validate cache
-    isCached = validateCache(args.wallpaper_path, args.blur, args.animate)
-
     # Run blurring script
-    blurWallpaper(args.wallpaper_path, args.blur, args.animate, isCached)
+    blurWallpaper(args.config_path, args.blur, args.animate, False)
 
 
 if __name__ == "__main__":
